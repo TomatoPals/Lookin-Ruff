9e829fb1fc092f03bbe2bb73f9225bbe44eddadc
G Strick <graysonstricker@gmail.com>
2020-12-16 11:19:29 -0500

    fixed bug with calendar incorrectly starting in october

--- public/js/calendarDBCalls.js
+++ public/js/calendarDBCalls.js
@@ -1,7 +1,13 @@
 $(document).ready(() => {
+  $("#appointmentfor").on("click", event => {
+    event.preventDefault();
+    console.log(`${event.target.id}`);
+  });
+  const serviceView = $("#appointmentfor");
+  const stylistView = $("#time");
+
   const getStylist = async () => await $.get("/api/stylist");
   const displayStylists = async response => {
-    const stylistView = $("#time");
     const stylistPromise = Promise.resolve(response);
     const stylistJSON = await stylistPromise;
     stylistJSON.forEach(stylist => {
@@ -12,4 +18,19 @@ $(document).ready(() => {
     console.log(stylistJSON);
   };
   displayStylists(getStylist());
+
+  const getService = async () => await $.get("/api/services");
+  const displayServices = async response => {
+    const servicePromise = Promise.resolve(response);
+    const serviceJSON = await servicePromise;
+    serviceJSON.forEach(service => {
+      serviceView.append(
+        `<option id="${service.id}" value="${service.description}">${service.description}</option>`
+      );
+    });
+    console.log(serviceJSON);
+  };
+  displayServices(getService());
+
+  // const bookAppointment
 });
