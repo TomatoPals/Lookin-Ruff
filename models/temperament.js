cb2b6432e559382c9077ee9d346504fe5241e655
Kerri Sexton <k.sexton804@gmail.com>
2020-12-17 20:14:59 -0500

    dropdown menu now displays properly

--- models/temperament.js
+++ models/temperament.js
@@ -1,12 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
-  const dogTemperaments = sequelize.define(
-    "dogTemperaments",
-    {
-      temperament: {
-        type: DataTypes.STRING,
-        allowNull: false
-      }
+  const dogTemperaments = sequelize.define("dogTemperaments", {
+    temperament: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  );
+  });
   return dogTemperaments;
 };
--- public/js/members.js
+++ public/js/members.js
@@ -18,42 +18,45 @@ $(document).ready(() => {
     $("#dogNote").val(data.dogNote);
   });
 
+  let clicked = false;
   let temperaments = [];
 
-  $("#temperamentBtn").on("click", temperamentMenu => {
+  $("#temperamentBtn").on("click", () => {
     $.get("/api/temperament", data => {
       temperaments = data;
       console.log(temperaments);
     }).then(() => {
       console.log("Temperaments: ", temperaments);
       //populate the returned temperments to the dropdown
-      for (let i = 0; i < temperaments.length; i++) {
-        $("<option/>")
-          .val(temperaments[i])
-          .text(temperaments[i])
-          .appendTo("#mydropdown");
-        console.log("Temperament: ", temperaments[i]);
+      if( clicked === false){
+        for (let i = 0; i < temperaments.length; i++) {
+
+          $("#myDropdown").append (
+          `<option id="${temperaments[i].id}" value="${temperaments[i].temperament}">${temperaments[i].temperament}</option>`)
+            clicked = true;
+          console.log("Temperament: ", temperaments[i].temperament);
+        }
       }
     });
 
     $("#myDropdown").toggle("show");
     event.preventDefault();
     console.log("myFunction is running");
-    filterFunction();
+    // filterFunction();
   });
 });
 
-filterFunction => {
-  const input = document.getElementById("myInput");
-  const filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  const a = div.getElementsByTagName("a");
-  for (let i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
-};
+// function filterFunction () {
+//   const input = document.getElementById("myInput");
+//   const filter = input.value.toUpperCase();
+//   div = document.getElementById("myDropdown");
+//   const a = div.getElementsByTagName("a");
+//   for (let i = 0; i < a.length; i++) {
+//     txtValue = a[i].textContent || a[i].innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       a[i].style.display = "";
+//     } else {
+//       a[i].style.display = "none";
+//     }
+//   }
+// };
