cb68339a9245285bd7a4e459cdcd4722d714a9fe
Kerri Sexton <k.sexton804@gmail.com>
2020-12-18 21:57:08 -0500

    readme and small changes

--- public/js/members.js
+++ public/js/members.js
@@ -31,32 +31,19 @@ $(document).ready(() => {
       if (clicked === false) {
         for (let i = 0; i < temperaments.length; i++) {
           $("#myDropdown").append(
-            `<option id="${temperaments[i].id}" value="${temperaments[i].temperament}">${temperaments[i].temperament}</option>`
+            `<option id="${temperaments[i].id}" value="${temperaments[i].temperament} class= "choice">${temperaments[i].temperament}</option>`
           );
           clicked = true;
           console.log("Temperament: ", temperaments[i].temperament);
         }
+        // $("option").onclick= function() {
+        //   console.log($(this));
+        // };
       }
     });
-
+    document.getElementById("option").onclick = function() {console.log('clicked: ')};
     $("#myDropdown").toggle("show");
     event.preventDefault();
     console.log("myFunction is running");
-    // filterFunction();
   });
 });
-
-// function filterFunction () {
-//   const input = document.getElementById("myInput");
-//   const filter = input.value.toUpperCase();
-//   div = document.getElementById("myDropdown");
-//   const a = div.getElementsByTagName("a");
-//   for (let i = 0; i < a.length; i++) {
-//     txtValue = a[i].textContent || a[i].innerText;
-//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//       a[i].style.display = "";
-//     } else {
-//       a[i].style.display = "none";
-//     }
-//   }
-// };
