bf32d9666ac1e6f92be3247b3619d953c30d399a
Patrick <patrick.lloyd@theproducebox.com>
2020-12-18 23:31:42 -0500

    final changes for singup page

--- public/js/members.js
+++ public/js/members.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+  console.log("$:", $);
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
